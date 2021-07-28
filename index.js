31aaa675fe73a89529e4c608de3c7a47a9148890
William Mosto <will.mosto@gmail.com>
2021-07-28 11:48:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input');
+const main = document.getElementById('main');
+
 function addingEventListener() {
+function clickAlert() {
+  alert('CLICK!');
+}
+input.addEventListener('click', clickAlert); 
+main.addEventListener('click',clickAlert);
 }
